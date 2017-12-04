a58858fbf3a6b3cf65f8e0897afc0ac602d95b0d
Antonio De Marco <antonio@nuvole.org>
2017-12-04 16:58:10 +0100

    Rollback to original patch.

--- src/Server.php
+++ src/Server.php
@@ -20,12 +20,14 @@ class Server extends Process
     {
         $this->port = $port;
         $this->host = $host;
+        $package_root = __DIR__ . '/../';
         parent::__construct(
             sprintf(
-                'exec php -dalways_populate_raw_post_data=-1 -derror_log= -S %s',
-                $this->getConnectionString()
+                'exec php -dalways_populate_raw_post_data=-1 -derror_log= -S %s -t public/ %spublic/index.php',
+                $this->getConnectionString(),
+                $package_root
             ),
-            realpath(__DIR__ . '/../public')
+            $package_root
         );
         $this->setTimeout(null);
     }
